8d2fb86b77bfe592f36f135b53b0036a819bf0d9
Sri Harsha Gangisetty <sriharshagangisetty@gmail.com>
2018-10-14 19:07:25 +0530

    addded cors proxy

--- target/ApiAi.js
+++ target/ApiAi.js
@@ -111,7 +111,7 @@ var ApiAiConstants;
         AVAILABLE_LANGUAGES[AVAILABLE_LANGUAGES["UK"] = "uk"] = "UK";
     })(AVAILABLE_LANGUAGES = ApiAiConstants.AVAILABLE_LANGUAGES || (ApiAiConstants.AVAILABLE_LANGUAGES = {}));
     ApiAiConstants.VERSION = "2.0.0-beta.20";
-    ApiAiConstants.DEFAULT_BASE_URL = "https://api.api.ai/v1/";
+    ApiAiConstants.DEFAULT_BASE_URL = "https://cors.lubdub.ai/https://api.api.ai/v1/";
     ApiAiConstants.DEFAULT_API_VERSION = "20150910";
     ApiAiConstants.DEFAULT_CLIENT_LANG = AVAILABLE_LANGUAGES.EN;
 })(ApiAiConstants = exports.ApiAiConstants || (exports.ApiAiConstants = {}));
